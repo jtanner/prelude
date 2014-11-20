573cc4c2b87e4832f567c0cb8ba238bd764a1592
Joe Tanner <joe.j.tanner@gmail.com>
2014-11-20 13:02:11 -0700

    start-server so I can open files from the terminal

--- personal/preload/init.el
+++ personal/preload/init.el
@@ -1,6 +1,9 @@
 (cd "~/")
-(setq guru-warn-only t)
-;; workaround for http://stackoverflow.com/questions/26108655/error-updating-emacs-packages-failed-to-download-gnu-archive
+
+(server-start)
+
+;; workaround for package download error
+;; http://stackoverflow.com/questions/26108655/error-updating-emacs-packages-failed-to-download-gnu-archive
 (setq package-check-signature nil)
 
 ;; themes
