925a9438f1a2c9d711dec53ad0733a047f7b3489
Joe Tanner <joe.j.tanner@gmail.com>
2015-04-01 19:40:56 -0600

    turn off undo-tree

---
+++ personal/undo-tree.el
@@ -0,0 +1,9 @@
+;; TURN IT OFF!
+(global-undo-tree-mode 0)
+(undo-tree-mode 0)
+(defun global-undo-tree-mode (_)
+  "Goodbye undo-tree-mode"
+  (interactive))
+(defun undo-tree-mode (_)
+  "Goodbye undo-tree-mode"
+  (interactive))
