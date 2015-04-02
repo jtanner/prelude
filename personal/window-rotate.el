e675259a47408e8a2ad685c2669ebd5706ef46a1
Joe Tanner <joe.j.tanner@gmail.com>
2015-04-01 19:42:42 -0600

    I like this window rotate fn better than prelude

---
+++ personal/window-rotate.el
@@ -0,0 +1,10 @@
+(defun rotate-windows-helper(x d)
+  (if (equal (cdr x) nil) (set-window-buffer (car x) d)
+    (set-window-buffer (car x) (window-buffer (cadr x))) (rotate-windows-helper (cdr x) d)))
+
+(defun rotate-windows ()
+  (interactive)
+  (rotate-windows-helper (window-list) (window-buffer (car (window-list))))
+  (select-window (car (last (window-list)))))
+
+(global-set-key (kbd "C-c s") 'rotate-windows)
